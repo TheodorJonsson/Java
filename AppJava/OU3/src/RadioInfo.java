65e4b5d9681bee7b4124bb55372d94b2d3622f6f
TheodorJonsson <129868890+TheodorJonsson@users.noreply.github.com>
2024-03-25 13:13:34 +0100

    Delete AppJava/OU3 directory

--- AppJava/OU3/src/RadioInfo.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-import radioGUI.*;
-
-public class RadioInfo {
-    public static void main(String[] args) {
-            SwingUtilities.invokeLater(()->new MainMenu());
-    }
-}
